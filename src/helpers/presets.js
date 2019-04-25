1466ccc8217237555f845a79063ffb8596f1a3cb
Edie Lemoine <edie@myparcel.nl>
2019-04-25 12:30:39 +0200

    MY-10341 update presets settings and add apps to config

--- src/helpers/presets.js
+++ src/helpers/presets.js
@@ -1,34 +1,4 @@
-import config from '../helpers/config';
 import { detect } from 'detect-browser';
-
-const BOLCOM = 'bolcom';
-const DRUPAL = 'drupal';
-const GRATISWEBSHOPBEGINNEN = 'gratiswebshopbeginnen';
-const JOOMLA = 'joomla';
-const LIGHTSPEED = 'lightspeed';
-const LUONDO = 'luondo';
-const MAGENTO1 = 'magento1';
-const MAGENTO2 = 'magento2';
-const MIJNWEBWINKEL = 'mijnwebwinkel';
-const MYSHOP = 'myshop';
-const OPENCART = 'opencart';
-const OSCOMMERCE = 'oscommerce';
-const PRESTASHOP = 'prestashop';
-const SEOSHOP = 'seoshop';
-const SHOPIFY = 'shopify';
-const SHOPPAGINA = 'shoppagina';
-const VIRTUEMART = 'virtuemart';
-const WOOCOMMERCE = 'woocommerce';
-
-/**
- * Fetch presets from presets json file.
- *
- * @return {Promise<Object>}
- */
-const getPresetFields = async() => {
-  const data = await fetch(chrome.extension.getURL(config.presetsFile));
-  return data.json();
-};
 
 export default {
 
@@ -46,29 +16,232 @@ export default {
    * Find existing preset by given URL.
    *
    * @param {string} url - URL.
-   * @return {*|string|boolean}
+   * @return {string|boolean}
    */
-  findPresetByURL(url) {
-    if (url.includes('.myshopify.com')) {
-      return SHOPIFY;
-    }
-
-    if (url.includes('/wp-admin/')) {
-      return WOOCOMMERCE;
-    }
-
-    return false;
+  findByURL(url) {
+    return Presets.urlMapping.find((entry) => entry.url.includes(url));
   },
 
   /**
    * Get preset data by preset name.
    *
    * @param {string} preset - Preset name.
+   *
    * @return {Promise<Object>}
    */
-  async getPresetData(preset) {
-    const fields = await getPresetFields();
+  getData(preset) {
+    const {fields} = Presets;
 
     return fields[preset];
   },
 };
+
+class Presets {
+  static BOLCOM = 'bolcom';
+  static DRUPAL = 'drupal';
+  static GRATISWEBSHOPBEGINNEN = 'gratiswebshopbeginnen';
+  static JOOMLA = 'joomla';
+  static LIGHTSPEED = 'lightspeed';
+  static LUONDO = 'luondo';
+  static MAGENTO1 = 'magento1';
+  static MAGENTO2 = 'magento2';
+  static MIJNWEBWINKEL = 'mijnwebwinkel';
+  static MYSHOP = 'myshop';
+  static OPENCART = 'opencart';
+  static OSCOMMERCE = 'oscommerce';
+  static PRESTASHOP = 'prestashop';
+  static SEOSHOP = 'seoshop';
+  static SHOPIFY = 'shopify';
+  static SHOPPAGINA = 'shoppagina';
+  static VIRTUEMART = 'virtuemart';
+  static WOOCOMMERCE = 'woocommerce';
+
+  /**
+   * Mapping for detecting presets by URL. `presets.findPresetByURL` will check if `url` is present in the given href.
+   *
+   * @type {Array}
+   */
+  static urlMapping = [
+    {
+      url: '/wp-admin/',
+      platform: this.WOOCOMMERCE,
+    },
+    {
+      url: '.myshopify.com',
+      platform: this.SHOPIFY,
+    },
+  ];
+
+  /**
+   * Preset field selectors for every platform.
+   *
+   * @type {{}[]}
+   */
+  static fields = [
+    {
+      [this.BOLCOM]: {
+        name: '',
+        company: '',
+        street: '',
+        number: '',
+        numberSuffix: '',
+        postalCode: '',
+        city: '',
+      },
+      [this.DRUPAL]: {
+        name: '',
+        company: '',
+        street: '',
+        number: '',
+        numberSuffix: '',
+        postalCode: '',
+        city: '',
+      },
+      [this.GRATISWEBSHOPBEGINNEN]: {
+        name: '',
+        company: '',
+        street: '',
+        number: '',
+        numberSuffix: '',
+        postalCode: '',
+        city: '',
+      },
+      [this.JOOMLA]: {
+        name: '',
+        company: '',
+        street: '',
+        number: '',
+        numberSuffix: '',
+        postalCode: '',
+        city: '',
+      },
+      [this.LIGHTSPEED]: {
+        name: '',
+        company: '',
+        street: '',
+        number: '',
+        numberSuffix: '',
+        postalCode: '',
+        city: '',
+      },
+      [this.LUONDO]: {
+        city: ':nth-child(3) > .general > tbody > :nth-child(5) > :nth-child(2)',
+        company: ':nth-child(3) > .general > tbody > :nth-child(1) > :nth-child(2)',
+        country: ':nth-child(3) > .general > tbody > :nth-child(6) > :nth-child(2)',
+        name: ':nth-child(3) > .general > tbody > :nth-child(2) > :nth-child(2)',
+        number: ':nth-child(3) > .general > tbody > :nth-child(3) > :nth-child(2)',
+        numberSuffix: ':nth-child(3) > .general > tbody > :nth-child(3) > :nth-child(2)',
+        postalCode: ':nth-child(3) > .general > tbody > :nth-child(4) > :nth-child(2)',
+        street: ':nth-child(3) > .general > tbody > :nth-child(3) > :nth-child(2)',
+      },
+      [this.MAGENTO1]: {
+        name: '',
+        company: '',
+        street: '',
+        number: '',
+        numberSuffix: '',
+        postalCode: '',
+        city: '',
+      },
+      [this.MAGENTO2]: {
+        name: '',
+        company: '',
+        street: '',
+        number: '',
+        numberSuffix: '',
+        postalCode: '',
+        city: '',
+      },
+      [this.MIJNWEBWINKEL]: {
+        name: '',
+        company: '',
+        street: '',
+        number: '',
+        numberSuffix: '',
+        postalCode: '',
+        city: '',
+      },
+      [this.MYSHOP]: {
+        name: '',
+        company: '',
+        street: '',
+        number: '',
+        numberSuffix: '',
+        postalCode: '',
+        city: '',
+      },
+      [this.OPENCART]: {
+        name: '',
+        company: '',
+        street: '',
+        number: '',
+        numberSuffix: '',
+        postalCode: '',
+        city: '',
+      },
+      [this.OSCOMMERCE]: {
+        name: '',
+        company: '',
+        street: '',
+        number: '',
+        numberSuffix: '',
+        postalCode: '',
+        city: '',
+      },
+      [this.PRESTASHOP]: {
+        name: '',
+        company: '',
+        street: '',
+        number: '',
+        numberSuffix: '',
+        postalCode: '',
+        city: '',
+      },
+      [this.SEOSHOP]: {
+        name: '',
+        company: '',
+        street: '',
+        number: '',
+        numberSuffix: '',
+        postalCode: '',
+        city: '',
+      },
+      [this.SHOPIFY]: {
+        name: '.ui-type-container > div > :nth-child(1) > a',
+        company: ':nth-child(4) > :nth-child(1) > .ui-type-container > .ui-stack > .ui-stack-item--fill > p',
+        street: '.ui-type-container > .ui-stack > .ui-stack-item--fill > p',
+        number: '.ui-type-container > .ui-stack > .ui-stack-item--fill > p',
+        numberSuffix: '.ui-type-container > .ui-stack > .ui-stack-item--fill > p',
+        postalCode: '.ui-type-container > .ui-stack > .ui-stack-item--fill > p',
+        city: '.ui-type-container > .ui-stack > .ui-stack-item--fill > p',
+      },
+      [this.SHOPPAGINA]: {
+        name: '',
+        company: '',
+        street: '',
+        number: '',
+        numberSuffix: '',
+        postalCode: '',
+        city: '',
+      },
+      [this.VIRTUEMART]: {
+        name: '',
+        company: '',
+        number: '',
+        numberSuffix: '',
+        postalCode: '',
+        city: '',
+      },
+      [this.WOOCOMMERCE]: {
+        name: ':nth-child(3) > .address > p@1',
+        company: ':nth-child(3) > .address > p@0',
+        street: ':nth-child(3) > .address > p@2',
+        number: ':nth-child(3) > .address > p@2',
+        numberSuffix: ':nth-child(3) > .address > p@2',
+        postalCode: ':nth-child(3) > .address > p@3',
+        city: ':nth-child(3) > .address > p@3',
+      },
+    },
+  ];
+}
+

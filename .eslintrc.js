ef897b176753dbb9baaf9fea2d68545990a8fc94
Edie Lemoine <edie@myparcel.nl>
2019-04-25 13:43:26 +0200

    MY-10341 update babel config to support classes

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,9 @@ module.exports = {
     jquery: true,
     jest: true,
   },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
   extends: ['../myparcel-core/dmp-standards/eslint/eslint-es6.config.js'],
 };
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -23,6 +23,21 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-transform-runtime',
+              '@babel/plugin-transform-named-capturing-groups-regex',
+            ],
+          },
+        },
+      },
+      {
         test: /\.scss$/,
         use: [
           {

7a63f3bfb65e647b8728e9dde3fe9220aa5c7f9c
Edie Lemoine <edie@myparcel.nl>
2019-04-27 12:19:04 +0200

    MY-10341 update logger

---
+++ src/helpers/Logger.js
@@ -0,0 +1,156 @@
+/* eslint-disable no-console */
+
+/**
+ * Debug logging file. This is only imported on development.
+ */
+export default class Logger {
+
+  /**
+   * General success message.
+   *
+   * @param {string} message - Message to output.
+   */
+  static success(message) {
+    this.createMessage(message, 'background-color: #23b237;');
+  }
+
+  /**
+   * General info message.
+   *
+   * @param {string} message - Message to output.
+   */
+  static info(message) {
+    this.createMessage(message, 'background-color: #34b7d4;');
+  }
+
+  /**
+   * General warning message.
+   *
+   * @param {string} message - Message to output.
+   */
+  static warning(message) {
+    this.createMessage(message, 'background-color: #d18800;');
+  }
+
+  /**
+   * General error message.
+   *
+   * @param {string} message - Message to output.
+   */
+  static error(message) {
+    this.createMessage(message, 'background-color: #d45839;');
+  }
+
+  /**
+   * Event message.
+   *
+   * @param {string} message - Message to output.
+   */
+  static event(message) {
+    console.log(
+      `%cEvent%c${message}`,
+      'background-color: #7842FF; color: white; border-radius: 2px 0 0 2px; padding: 0 .5rem; font-size: 85%;',
+      'background-color: #fff; color: #222; border-radius: 0 2px 2px 0; padding: 0 .5rem; font-size: 85%;',
+    );
+  }
+
+  /**
+   * Message to or from popup.
+   *
+   * @param {string} message - Message to output.
+   * @param {boolean} receiving - Whether the message is inbound or outbound.
+   */
+  static popup(message, receiving = false) {
+    console.log(
+      `%c${this.receiving(receiving)}%cPopup%c${message}`,
+      `${this.color(receiving)} border-radius: 2px 0 0 2px; padding: 1px .5rem;`,
+      // eslint-disable-next-line max-len
+      `background: linear-gradient(to ${receiving ? 'top' : 'bottom'} left, red, #ff8c00); color: white; padding: 1px .5rem;`,
+      `${this.color(receiving)} border-radius: 0 2px 2px 0; padding: 1px .5rem;`,
+    );
+  }
+
+  /**
+   * Message to or from content.
+   *
+   * @param {string} message - Message to output.
+   * @param {boolean} receiving - Whether the message is inbound or outbound.
+   */
+  static content(message, receiving = false) {
+    console.log(
+      `%c${this.receiving(receiving)}%cContent%c${message}`,
+      `${this.color(receiving)} border-radius: 2px 0 0 2px; padding: 1px .5rem;`,
+      // eslint-disable-next-line max-len
+      `background: linear-gradient(to ${receiving ? 'top' : 'bottom'} left, blue, #1eb9c5); color: white; padding: 1px .5rem;`,
+      `${this.color(receiving)} border-radius: 0 2px 2px 0; padding: 1px .5rem;`,
+    );
+  }
+
+  /**
+   * Message to or from background.
+   *
+   * @param {string} message - Message to output.
+   * @param {boolean} receiving - Whether the message is inbound or outbound.
+   */
+  static background(message, receiving = false) {
+    console.log(
+      `%c${this.receiving(receiving)}%cBackground%c${message}`,
+      `${this.color(receiving)} border-radius: 2px 0 0 2px; padding: 1px .5rem;`,
+      // eslint-disable-next-line max-len
+      `background: linear-gradient(to ${receiving ? 'top' : 'bottom'} left, yellowgreen, #1eb436); color: white; padding: 1px .5rem;`,
+      `${this.color(receiving)} border-radius: 0 2px 2px 0; padding: 1px .5rem;`,
+    );
+  }
+
+  /**
+   * Log a request from or to a connection Port.
+   *
+   * @param {string} type - Corresponds to function name.
+   * @param {Object} request - Request object.
+   * @param {boolean} receiving - Whether the message is inbound or outbound.
+   *
+   * @example Logger.request('background', data, true); // Creates a background-style inbound message.
+   * @example Logger.request('popup', data); // Creates a popup-style outbound message.
+   */
+  static request(type, request, receiving = false) {
+    this[type](request.action, receiving);
+    console.log(request);
+  }
+
+  /**
+   * Set inbound, outbound or queue message color.
+   *
+   * @param {boolean|string} bool - Boolean or 'queue'.
+   * @return {string} - CSS.
+   */
+  static color(bool) {
+    if (bool === 'queue') {
+      return 'background-color: #666; color: #fff;';
+    }
+    return bool ? 'background-color: #fff; color: #222;' : 'background-color: #333; color: #fff;';
+  }
+
+  /**
+   * Whether a message is inbound or outbound. Can also be used to mark a message being queued.
+   *
+   * @param {boolean|string} bool - Boolean or 'queue'.
+   *
+   * @return {string}
+   */
+  static receiving(bool) {
+    if (bool === 'queue') {
+      return 'QUEUED';
+    }
+    return bool ? '▼' : '▲';
+  }
+
+  /**
+   * Log a message.
+   *
+   * @param {string} message - Message.
+   * @param {string} style - CSS to append to the default.
+   */
+  static createMessage(message, style = '') {
+    console.log(`%c${message}`, `color: white; ${style} border-radius: 2px; padding: 1px .5em;`);
+  }
+}

40adefd7c01eaac056362e09ab57e5320839975c
Edie Lemoine <edie@myparcel.nl>
2019-05-27 16:09:28 +0200

    MY-10341 update staging config

--- build/webpack.stage.conf.js
+++ build/webpack.stage.conf.js
@@ -1,9 +1,19 @@
-/* eslint-disable no-magic-numbers,no-console */
-const merge = require('webpack-merge');
 const prodConfig = require('./webpack.prod.conf.js');
 
 module.exports = (env, argv) => {
-  return merge(prodConfig(env, argv), {
+  const config = [];
+
+  const stageConfig = {
     devtool: 'eval-source-map',
+  };
+
+  // Merge stage configuration into all prod configs.
+  prodConfig(env, argv).forEach((prodConfig) => {
+    config.push({
+      ...prodConfig,
+      ...stageConfig,
+    });
   });
+
+  return config;
 };
